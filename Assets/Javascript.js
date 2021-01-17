c9f1f5ca282f99d57c3bdbf88045574ca3487f50
Mr. Spencer <codekingkyle@gmail.com>
2021-01-17 16:29:48 -0500

    i think i have everything but need to make it talk to eachother

--- Assets/Javascript.js
+++ Assets/Javascript.js
@@ -1,6 +1,6 @@
 // VARIABLES
 
-var currentDate = $("#todaysDate");
+var today = $("#todaysDate");
 var scheduleContent = $("scheduelcontent");
 var timeRow = $("hourByHour");
 var currentDate = moment().format("dddd, MMMM Do");
@@ -8,7 +8,6 @@ var currentHour = moment().format("H");
 var userContent = [];
 
 // save function
-
 function saveData (){
     var hourToUpdate = $(this).parent().attr("data-time");
     var itemsToAdd = (($(this).parent()).children("textarea")).val();
@@ -20,11 +19,6 @@ function saveData (){
     localStorage.setItem("userToDos", JSON.stringify(userContent));
     renderSchedule();
 }
-
-
-
-
-
 
 
 // declare new function
@@ -43,4 +37,33 @@ function startSchedule() {
     // save to storage afer loops 
     localStorage.setItem("userToDos", JSON.stringify(userContent))
 };
+
+// getting arrays or pull from local 
+
+function renderSchedule() {
+
+    userContent = localStorage.getItem("userToDos");
+    userContent = JSON.parrse(userContent);
+
+    for (var i = 0; i <userContent.length; i++){
+        var itemHour = userContent[i].hour;
+        var itemText = userContent[i].text;
+        $("[data-time=" + itemHour + "]").childre("textarea").val(itemText);
+    }
+}
+
+$(document).ready(function(){
+    setUpRows();
+
+    if (!localStorage.getItem("userToDos")){
+        startSchedule();
+    }
+
+    today.text(currentDate);
+
+    renderSchedule();
+
+    scheduleArea.on("click","button", saveIt);
+});
+
 
